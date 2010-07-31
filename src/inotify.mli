fcc81d07a74a2cd6191144782acb159109bcb7d5
Ludovic Stordeur <ludovic@okazoo.eu>
2010-07-31 16:27:22 +0200

    Intiated some API cleanups and documentation
    API cleanups :
        Merged select_event and type_event to ev_type
        Moved the event type to a record and renamed it ev

--- src/inotify.mli
+++ src/inotify.mli
@@ -13,54 +13,64 @@
  *
  * Inotify OCaml binding
  *)
+
+(** @author Vincent Hanquez
+    @author Ludovic Stordeur *)
+
 exception Error of string * int
 
-type select_event =
-| S_Access
-| S_Attrib
-| S_Close_write
-| S_Close_nowrite
-| S_Create
-| S_Delete
-| S_Delete_self
-| S_Modify
-| S_Move_self
-| S_Moved_from
-| S_Moved_to
-| S_Open
-| S_Dont_follow
-| S_Mask_add
-| S_Oneshot
-| S_Onlydir
-| S_Move
-| S_Close
-| S_All
+(** Available events *)
+type ev_type =
+| Access	(** Read access to the file *)
+| Attrib	(** Metadata modification *)
+| Close_write	(** Write-opened file closed *)
+| Close_nowrite (** Non write-opened file closed *)
+| Create	(** File created under the watched directory *)
+| Delete	(** File deleted under the watched directory *)
+| Delete_self	(** Watched file deleted *)
+| Modify	(** File modified *)
+| Move_self	(** Watched file moved *)
+| Moved_from	(** File moved outside of the watched directory *)
+| Moved_to	(** File moved inside the watched directory *)
+| Open		(** File opened *)
+| All		(** All ABOVE events *)
 
-type type_event =
-| Access
-| Attrib
-| Close_write
-| Close_nowrite
-| Create
-| Delete
-| Delete_self
-| Modify
-| Move_self
-| Moved_from
-| Moved_to
-| Open
-| Ignored
-| Isdir
-| Q_overflow
-| Unmount
+| Move		(** Moved_from and Moved_to events *)
+| Close		(** Close_write and Close_nowrite events *)
 
+| Dont_follow	(** Do not dereference watched file if it is a symbolic
+		    link. add_watch() only *)
+| Mask_add	(** Add events to (instead of replacing) existing ones.
+		    add_watch() only *)
+| Oneshot	(** Watch the file until the first event.
+		    add_watch() only *)
+| Onlydir	(** Watch the file only if it is a directory *)
+
+| Ignored	(** The watched file has been removed explicity
+		    (rm_watch()) or automatically (file deleted or
+		    filesystem unmounted). read() only *)
+| Isdir		(** Event subject is a directory. read() only *)
+| Q_overflow	(** Event queue overloaded. read() only *)
+| Unmount	(** Filesystem containing the watched file has been
+		    unmounted. read() only *)
+
+(** A watch descriptor *)
 type wd
-type event = wd * type_event list * int32 * string option
 
-val int_of_wd : wd -> int
-val string_of_event : type_event -> string
+(** Define an event *)
+type ev = { wd	   : wd;	    (** The associated watch descriptor *)
+	    evs    : ev_type list;  (** List of events *)
+	    cookie : int32;	    (** Unique identifier used to bind
+					events together. Currently only
+					used to bind Move_from and Move_to *)
+	    name   : string option  (** Optional name associated to the event *) }
+    
 
-val init : unit -> Unix.file_descr
-val add_watch : Unix.file_descr -> string -> select_event list -> wd
-val rm_watch : Unix.file_descr -> wd -> unit
-val read : Unix.file_descr -> event list
+(* val int_of_wd : wd -> int *)
+
+val string_of_ev_type : ev_type -> string
+
+val init      : unit -> Unix.file_descr
+val add_watch : Unix.file_descr -> string -> ev_type list -> wd
+val rm_watch  : Unix.file_descr -> wd -> unit
+val read      : Unix.file_descr -> ev list
