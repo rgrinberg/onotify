2f5bb8ffefe71832b23db8762cefe517e4edb006
Ludovic Stordeur <ludovic@okazoo.eu>
2010-08-07 19:46:49 +0200

    Added byte build support for tests

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,6 +4,9 @@
  
  let _ =  dispatch begin function
      | After_rules ->
-             dep ["link"; "ocaml"; "native"; "use_stubs"] ["src/stubs/libinotify_stubs.a"];
+             dep  ["link"; "ocaml"; "native"; "use_stubs"] ["src/stubs/libinotify_stubs.a"];
+
+             flag ["link"; "ocaml"; "byte"; "use_stubs"]   (A"-custom");
+             dep  ["link"; "ocaml"; "byte"; "use_stubs"]   ["src/stubs/libinotify_stubs.a"];
      | _ -> ()
  end
