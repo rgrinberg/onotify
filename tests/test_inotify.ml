82daf99dffc95c7ce4c94b5edeca4cde678032a4
Ludovic Stordeur <ludovic@okazoo.eu>
2010-09-11 18:43:48 +0200

    Improve test_inotify.ml
    Improve test_inotify to make it a general testing tool
    for the library

--- tests/test_inotify.ml
+++ tests/test_inotify.ml
@@ -10,17 +10,19 @@ let _ =
 	end;
     
     let fd = Inotify.init () in
-    ignore (Inotify.add_watch fd Sys.argv.(1) [ Inotify.R_Create ]);
+    
+    let wd =  Inotify.add_watch fd Sys.argv.(1) [ Inotify.R_All ] in
+    printf "Retrieved watch descriptor : %d\n%!" wd;
 
     let string_of_ev ev =
 	let wd,mask,cookie,name = (ev.Inotify.wd,
 				   ev.Inotify.mask,
 				   ev.Inotify.cookie,
 				   ev.Inotify.name) in
-	let mask = String.concat ":" (List.map Inotify.string_of_bit mask) in
+	let mask = String.concat ":" (List.map Inotify.string_of_ev_type mask) in
 	let name = match name with
 	    | Some s -> s
-	    | None -> "\"\"" in
+	    | None   -> "\"\"" in
 	sprintf "wd[%d] mask[%s] cookie[%ld] %s" wd mask cookie name
 	
     in
