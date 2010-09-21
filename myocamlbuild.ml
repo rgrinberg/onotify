b3c045941585f782c9dc589e0e103e534444fc12
Ludovic Stordeur <ludovic@okazoo.eu>
2010-09-21 19:41:20 +0200

    Use the autolink system to link the stub library

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -8,5 +8,8 @@
 
              flag ["link"; "ocaml"; "byte"; "use_stubs"]   (A"-custom");
              dep  ["link"; "ocaml"; "byte"; "use_stubs"]   ["src/stubs/libinotify_stubs.a"];
+
+	     flag ["link"; "ocaml"; "byte"; "stubflags"]   (S[A"-custom"; A"-cclib"; A"-linotify_stubs"]);
+	     flag ["link"; "ocaml"; "native"; "stubflags"] (S[A"-cclib";  A"-linotify_stubs"]);
      | _ -> ()
  end
