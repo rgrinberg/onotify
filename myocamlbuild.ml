8ef46e272516b1261f35bc00e6d3e3ae67c41635
Ludovic Stordeur <ludovic@okazoo.eu>
2010-10-12 01:34:53 +0200

    Improved the documentation generation
    Pass the -colorize-code option when generating the documentation.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,12 +4,13 @@
  
  let _ =  dispatch begin function
      | After_rules ->
-             dep  ["link"; "ocaml"; "native"; "use_stubs"] ["src/stubs/libinotify_stubs.a"];
+           dep  ["link"; "ocaml"; "use_stubs"]		 ["src/stubs/libinotify_stubs.a"];
 
-             flag ["link"; "ocaml"; "byte"; "use_stubs"]   (A"-custom");
-             dep  ["link"; "ocaml"; "byte"; "use_stubs"]   ["src/stubs/libinotify_stubs.a"];
-
-	     flag ["link"; "ocaml"; "byte"; "stubflags"]   (S[A"-custom"; A"-cclib"; A"-linotify_stubs"]);
-	     flag ["link"; "ocaml"; "native"; "stubflags"] (S[A"-cclib";  A"-linotify_stubs"]);
+           flag ["link"; "ocaml"; "byte"; "use_stubs"]   (A"-custom");
+	   
+	   flag ["link"; "ocaml"; "byte"; "stubflags"]   (S[A"-custom"; A"-cclib"; A"-linotify_stubs"]);
+	   flag ["link"; "ocaml"; "native"; "stubflags"] (S[A"-cclib";  A"-linotify_stubs"]);
+	   
+	   flag ["doc"; "ocaml"]			 (S[A"-colorize-code"])
      | _ -> ()
  end
