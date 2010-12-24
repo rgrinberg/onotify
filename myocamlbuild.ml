5255596b5e4cf3446840646ec4a501323152f049
Ludovic Stordeur <ludovic@okazoo.eu>
2010-12-24 21:41:10 +0100

    Removed the ocamlbuild stubflags
    Removed the stubflags. Instead, file/src/inotify.cm{x}a flags have
    been hardcoded in the relevant ocamlbuild rules.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,14 +4,17 @@ open Command
 
 let _ =  dispatch begin function
     | After_rules ->
-        dep  ["link"; "ocaml"; "use_stubs"]	      ["src/stubs/libinotify_stubs.a"];
+	    (* Handle the use_stubs flag *)
+        dep  ["link"; "ocaml"; "use_stubs"]		["src/stubs/libinotify_stubs.a"];
+        flag ["link"; "ocaml"; "byte"; "use_stubs"]	(A"-custom");
 
-        flag ["link"; "ocaml"; "byte"; "use_stubs"]   (A"-custom");
+	    (* Add the stub linking flags when building library *)
+	flag ["link"; "ocaml"; "byte";
+	      "file:src/inotify.cma"]			(S[A"-custom"; A"-cclib"; A"-linotify_stubs"]);
+	flag ["link"; "ocaml"; "native";
+	      "file:src/inotify.cmxa"]			(S[A"-cclib";  A"-linotify_stubs"]);
 
-	flag ["link"; "ocaml"; "byte"; "stubflags"]   (S[A"-custom"; A"-cclib"; A"-linotify_stubs"]);
-	flag ["link"; "ocaml"; "native"; "stubflags"] (S[A"-cclib";  A"-linotify_stubs"]);
-
-	flag ["doc"; "ocaml"]			      (S[A"-colorize-code"])
+	flag ["doc"; "ocaml"]				(S[A"-colorize-code"])
     | _ -> ()
 end
 
